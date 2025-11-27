bd0b30c587347535f5d7b6c8d38955fb61304592
Real1ty <vavro.matej3@gmail.com>
2025-11-27 12:34:19 +0100

    chore: enhance ESLint configuration and add new linting scripts for ESLint

--- eslint.config.mjs
+++ eslint.config.mjs
@@ -4,29 +4,28 @@ import { defineConfig } from "eslint/config";
 import obsidianmd from "eslint-plugin-obsidianmd";
 
 export default defineConfig([
-  ...obsidianmd.configs.recommended,
-  {
-    ignores: [
-      "node_modules/**",
-      "**/node_modules/**",
-      "docs-site/**",
-      "**/dist/**",
-      "**/build/**",
-      "main.js",
-      "*.config.js",
-      "*.config.mjs",
-      "esbuild.config.mjs",
-      "version-bump.mjs",
-    ],
-  },
-  {
-    files: ["**/*.ts"],
-    languageOptions: {
-      parser: tsparser,
-      parserOptions: {
-        project: "./tsconfig.json",
-      },
-    },
-
-  },
+	...obsidianmd.configs.recommended,
+	{
+		ignores: [
+			"node_modules/**",
+			"**/node_modules/**",
+			"docs-site/**",
+			"**/dist/**",
+			"**/build/**",
+			"main.js",
+			"*.config.js",
+			"*.config.mjs",
+			"esbuild.config.mjs",
+			"version-bump.mjs",
+		],
+	},
+	{
+		files: ["**/*.ts"],
+		languageOptions: {
+			parser: tsparser,
+			parserOptions: {
+				project: "./tsconfig.json",
+			},
+		},
+	},
 ]);
